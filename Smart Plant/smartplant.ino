c944940d9ece3979adbcf4ee2a9dbcec65c9ecd4
Shengrui Cen <cenrui.93@gmail.com>
2019-11-07 06:13:22 -0500

    smart plant working version

---
+++ Smart Plant/smartplant.ino
@@ -0,0 +1,245 @@
+/**
+ * Mario Secion BEGIN
+ */
+#include "pitches.h"
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+ 
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+
+//Mario main them tempo
+int tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+//Underworld melody
+int underworld_melody[] = {
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+  NOTE_CS4, NOTE_DS4,
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  0, 0, 0
+};
+//Underwolrd tempo
+int underworld_tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  6, 18, 18, 18,
+  6, 6,
+  6, 6,
+  6, 6,
+  18, 18, 18, 18, 18, 18,
+  10, 10, 10,
+  10, 10, 10,
+  3, 3, 3
+};
+/**
+ * Mario Section END
+ */
+
+int speakerPin = D5;
+// int forcePin = A2;//TODO:
+
+// int forceReading = 0;//TODO
+// create an array for the notes in the melody:
+//C4,G3,G3,A3,G3,0,B3,C4
+int song = 0;
+int melody_song = 1;
+int underworld_melody_song = 2;
+ 
+
+
+
+
+// int DIN = D5;
+// int CS = D6;
+// int CLK = D7;
+
+// byte smile[8] = {0x3C, 0x42, 0xA5, 0x81, 0xA5, 0x99, 0x42, 0x3C};
+//Define the pin for photo cell
+int photoCellPin = A0;
+int switchPin = A1;
+//create a variable to store light reading
+int photoCellReading = 0;
+
+//Define LED pin
+int ledPinGreen = D2;
+int ledPinBlue = D3;
+
+//Define the pin for temperature
+// int tempPin = A1;
+// 
+//create a variable to store LED brightness
+int ledBrightness = 0;
+int forceReading = 0;
+//create variables to store temperature value
+int tempC = 0;
+int tempF = 0;
+// LedControl lc = LedControl(DIN, CLK, CS, 0);
+int timer = 0;
+void setup() {
+    // pinMode(tempPin, INPUT);
+    // pinMode(btnPinBlue, INPUT);
+    // pinMode(btnPinGreen, INPUT);
+    pinMode(ledPinGreen, OUTPUT);
+    pinMode(ledPinBlue, OUTPUT);
+    pinMode(switchPin, INPUT_PULLUP); // sets pin as input
+
+    // Register a Particle variable for light reading
+    Particle.variable("light", &photoCellReading, INT);
+    Particle.variable("force", &forceReading, INT);
+    // Register a Particle variable for temperature reading
+    // Particle.variable("tempC", &tempC, DOUBLE);
+    // Particle.variable("tempF", &tempF, DOUBLE);
+    
+    // lc.shutdown(0, false);
+    // lc.setIntensity(0, 15);
+    // lc.clearDisplay(0);
+}
+
+
+
+void loop() {
+  photoCellReading = analogRead(photoCellPin);
+  int switchState = digitalRead(switchPin);
+
+    if (switchState == HIGH) {
+        sing(melody_song);
+    } else {
+        noTone(speakerPin);
+    }
+    
+    if (timer <= 0) {
+        //collect data each 15 minute
+        timer = 60000*15;
+        Particle.publish("light_value", String(photoCellReading));
+        if (photoCellReading < 200) {
+            digitalWrite(ledPinBlue, HIGH);
+            digitalWrite(ledPinGreen, LOW);
+        } else {
+            digitalWrite(ledPinGreen, HIGH);
+            digitalWrite(ledPinBlue, LOW);
+        }
+    }
+    timer = timer - 1000;
+    delay(1000);
+}
+
+//A helper function that sing a sone
+void sing(int s) {
+  // iterate over the notes of the melody:
+  song = s;
+  if (song == 2) {
+    // Serial.println(" 'Underworld Theme'");
+    int size = sizeof(underworld_melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+ 
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / underworld_tempo[thisNote];
+ 
+      tone(speakerPin, underworld_melody[thisNote], noteDuration);
+      analogWrite(ledPinGreen, underworld_melody[thisNote]);
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+ 
+      // stop the tone playing:
+      tone(speakerPin, 0, noteDuration);
+
+ 
+    }
+ 
+  } else {
+ 
+    int size = sizeof(melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+ 
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / tempo[thisNote];
+ 
+      tone(speakerPin, melody[thisNote], noteDuration);
+      analogWrite(ledPinGreen, melody[thisNote]);
+
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+ 
+      // stop the tone playing:
+      tone(speakerPin, 0, noteDuration);
+ 
+    }
+  }
+}
